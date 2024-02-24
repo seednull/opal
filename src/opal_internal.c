337abc1249a51c933ad3ed26f422145a291f9906
Andrey Viktorov <andrey.viktorov@gmail.com>
2024-02-25 02:58:20 +0700

    Oops

--- src/opal_internal.c
+++ src/opal_internal.c
@@ -34,9 +34,9 @@ uint32_t opalEvaluateDevice(const Opal_DeviceInfo *info, Opal_DeviceHint hint)
 
 	switch (hint)
 	{
-		case OPAL_DEVICE_HINT_PREFER_HIGH_PERFORMANCE: score += gpu_type_high_performance_scores[info->gpu_type];
-		case OPAL_DEVICE_HINT_PREFER_LOW_POWER: score += gpu_type_low_power_scores[info->gpu_type];
-		default: score += gpu_type_default_scores[info->gpu_type];
+		case OPAL_DEVICE_HINT_PREFER_HIGH_PERFORMANCE: score += gpu_type_high_performance_scores[info->gpu_type]; break;
+		case OPAL_DEVICE_HINT_PREFER_LOW_POWER: score += gpu_type_low_power_scores[info->gpu_type]; break;
+		default: score += gpu_type_default_scores[info->gpu_type]; break;
 	}
 
 	score += info->tessellation_shader;
