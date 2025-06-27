24013afd437447dba3b2fa56491342381ed99299
Andrey Viktorov <andrey.viktorov@gmail.com>
2025-06-28 01:10:42 +0700

    [DirectX 12] Added note for future fix

--- src/directx12/directx12_internal.h
+++ src/directx12/directx12_internal.h
@@ -171,7 +171,7 @@ typedef struct DirectX12_Texture_t
 
 typedef struct DirectX12_TextureView_t
 {
-	ID3D12Resource *texture;
+	ID3D12Resource *texture; // TODO: bad design, better to store Opal_Texture handle
 	DXGI_FORMAT format;
 	UINT subresource_index;
 	D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc;
