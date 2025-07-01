e37e9bd4ddf0f7d2b229fa2d07987d24c5021866
Andrey Viktorov <andrey.viktorov@gmail.com>
2025-07-02 01:22:42 +0700

    [Samples] Fixed shader entry points for Vulkan

--- samples/04_triangle/shaders/vulkan/main.frag.glsl
+++ samples/04_triangle/shaders/vulkan/main.frag.glsl
@@ -3,7 +3,7 @@
 layout(location = 0) in vec4 in_color;
 layout(location = 0) out vec4 out_color;
 
-void main()
+void fragmentMain()
 {
 	out_color.rgb = pow(in_color.rgb, vec3(1.0f / 2.2f));
 	out_color.a = in_color.a;
--- samples/04_triangle/shaders/vulkan/main.vert.glsl
+++ samples/04_triangle/shaders/vulkan/main.vert.glsl
@@ -5,7 +5,7 @@ layout(location = 1) in vec4 in_color;
 
 layout(location = 0) out vec4 out_color;
 
-void main()
+void vertexMain()
 {
 	gl_Position = vec4(in_position.xyz, 1.0);
 	out_color = in_color;
